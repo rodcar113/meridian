206760c3c8ba1db4037634e44dca82066751b945
rodcar113 <mrcarrillo113@gmail.com>
2026-01-06 18:40:35 -0600

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-console.log("Meridian Delta Solutions - Sitio cargado correctamente");
+/* MENÚ HAMBURGUESA */
+const toggle = document.getElementById("menu-toggle");
+const nav = document.querySelector("nav");
+
+toggle.addEventListener("click", () => {
+    nav.classList.toggle("active");
+});
+
+/* ANIMACIONES ON SCROLL */
+const elements = document.querySelectorAll(".fade-in");
+
+const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add("visible");
+        }
+    });
+});
+
+elements.forEach(el => observer.observe(el));
